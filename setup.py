cfc8638b56b39b52a5b0fa92df0c45e8c8ded6cf
Damien Couroussé <damien.courousse@gmail.com>
2014-04-12 09:06:17 +0200

    cleanup: version.determine_path

--- setup.py
+++ setup.py
@@ -26,19 +26,6 @@ from distutils.command.build import build
 import version
 
 
-def determine_path():
-    """Borrowed from wxglade.py"""
-    try:
-        root = __file__
-        if os.path.islink (root):
-            root = os.path.realpath (root)
-        return os.path.dirname (os.path.abspath (root))
-    except:
-        print "I'm sorry, but something is wrong."
-        print "There is no __file__ variable. Please contact the author."
-        sys.exit ()
-
-
 class BuildVersion(build):
 
     def run(self):
